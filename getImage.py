bd400302f1c070018edc919da7e4b66653a5af0f
liyang <389688501@qq.com>
2023-04-27 19:15:49 +0800

    feat：init

---
+++ getImage.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+# 设置要爬取图片的 URL
+url = 'https://www.woyaogexing.com/touxiang/'
+
+# 发送 HTTP 请求并解析页面内容
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# 获取所有图片链接
+img_links = []
+# print(soup)
+for img in soup.find_all('img'):
+    img_links.append("https:"+img.get('src'))
+
+# 创建保存图片的文件夹
+# if not os.path.exists('images'):
+#     os.makedirs('images')
+# print(img_links)
+# 下载图片并保存到本地
+dir = "/Users/macosx/Desktop/项目文档/python-demo/Crawlmages/avatar"
+for link in img_links:
+    response = requests.get(link)
+    filename = link.split('/')[-1]
+    with open(f'{dir}/{filename}', 'wb') as f:
+        f.write(response.content)
---
+++ reptileYoutubeVideo.py
@@ -0,0 +1,44 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+# 设置保存视频的路径
+save_path = '/Users/macosx/Desktop/项目文档/python-demo/files/youtube-video'
+
+# 创建保存路径文件夹（如果不存在）
+os.makedirs(save_path, exist_ok=True)
+
+# YouTube 页面 URL
+url = 'https://www.youtube.com/results?search_query=高清风景'
+
+# 发起 HTTP 请求获取页面内容
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup)
+# 解析页面获取视频链接
+video_links = soup.select('.yt-uix-tile-link')
+
+# 遍历视频链接并下载
+for link in video_links:
+    video_url = 'https://www.youtube.com' + link['href']
+    video_id = link['href'][9:]
+    download_url = f'https://www.ssyoutube.com/watch?v={video_id}'
+
+    # 发起 HTTP 请求获取视频下载链接
+    response = requests.get(download_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # 解析页面获取视频下载链接
+    download_link = soup.select('.download-box a')[0]['href']
+
+    # 下载视频并保存到指定路径
+    video_response = requests.get(download_link, stream=True)
+    video_name = link.text.strip() + '.mp4'
+    video_path = os.path.join(save_path, video_name)
+
+    with open(video_path, 'wb') as f:
+        for chunk in video_response.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+
+    print(f'已下载保存视频：{video_name}')
