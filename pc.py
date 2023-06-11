f250c638ac440c2348bfb1d448977a34833a1194
liyang <389688501@qq.com>
2023-06-11 17:25:56 +0800

    feat：init

--- pc.py
+++ pc.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-dir = "/Users/macosx/Desktop/项目文档/-demo/Crawlmages/app_logos"
+dir = "/Users/macosx/Desktop/项目文档/python-demo/files/app_logos"
 # 创建保存图片的文件夹
 if not os.path.exists(dir):
     os.makedirs(dir)
@@ -19,6 +19,7 @@ apps = soup.find_all('li', class_='category-item bottom-shadow J_cat_item')
 for app in apps[:100]:
     # 获取应用名称和Logo URL
     app_name = app.find('h2', class_='category-name ellipsis').value
+    print(app_name)
     logo_url = 'https:' + app.find('img', class_='')['src']
     # newName = app_name.encode('utf-8')
     # 下载Logo图片并保存到本地文件夹
